ce2acd0bb18c0da1d7e88c3a0368198a757670d7
Sakib <nazmunsakib81@gmail.com>
2025-01-07 23:22:49 +0600

    Update login page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+  include "parts/header.php"; 
+  require_once __DIR__ . '/vendor/autoload.php';
+  ?>
+
+  <!-- Banner Start -->
+  <section class="cc-banner">
+      <div class="container">
+          <div class="cc-banner-content">
+              <h1><?php echo "Career Craft"; ?></h1>
+          </div>
+      </div>
+  </section>
+  <!-- Banner End -->
+
+  <!-- Job listing Start -->
+  <section class="cc-job-listing">
+      <div class="container">
+          <div class="cc-job-board-area">
+          <?php 
+            include "parts/filter.php"; 
+            include "parts/job-list.php"; 
+            ?>
+          </div>
+      </div>
+  </section>
+  <!-- Job listing End -->
+
+<?php include "parts/footer.php"; ?>
---
+++ login.php
@@ -0,0 +1,71 @@
+<?php include "parts/header.php"; 
+
+  
+//   use CareerCraft\Classes\User;
+//   $dd = new User();
+?>
+
+    <!-- Banner Start -->
+    <section class="cc-banner">
+        <div class="container">
+            <div class="cc-banner-content">
+                <h1><?php echo "Login / Register"; ?></h1>
+            </div>
+        </div>
+    </section>
+    <!-- Banner End -->
+
+    <!-- login card start -->
+    <section class="cc-login-section">
+        <div class="container">
+            <div class="cc-login-register-card">
+                <div class="cc-login-card">
+                    <div class="cc-login-card-inner">
+                        <h4><?php echo "Login"; ?></h4>
+                        <form action="includes/user-action.php" method="POST">
+                            <div class="form-group">
+                                <label for="username">User name</label>
+                                <input type="text" name="username" placeholder="User name">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" placeholder="Password">
+                            </div>
+                            <button id="login-button" name="login" type="submit">Login</button>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="cc-register-card">
+                    <div class="cc-login-card-inner">
+                        <h4><?php echo "Register"; ?></h4>
+                        <form action="includes/user-action.php" method="POST">
+                            <div class="form-group">
+                                <label for="name">Full Name</label>
+                                <input type="text" name="name" placeholder="Full Name">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" placeholder="Password">
+                            </div>
+                            <div class="form-cc-user-type">
+                                <label for="user_type">User Type</label>
+                                <select id="user_type" name="user_type">
+                                    <option value="employer">I am an Employer</option>
+                                    <option value="job-seeker">I am a Job Seeker</option>
+                                </select>
+                            </div>
+                            <button id="login-button" name="register" type="submit">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- login card End -->
+
+<?php include "parts/footer.php"; ?>
